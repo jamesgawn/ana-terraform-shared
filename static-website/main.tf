7622b0353a49d0f7871806a0cdf7b2aebd6d8d79
akyan <james@gawn.uk>
2018-10-20 23:42:58 +0100

    Added missing permission required to sync files to S3 bucket in code build

--- static-website/main.tf
+++ static-website/main.tf
@@ -142,7 +142,8 @@ resource "aws_iam_role_policy" "codebuild_policy" {
        "s3:PutObject",
        "s3:GetObject",
        "s3:GetObjectVersion",
-       "s3:GetBucketVersioning"
+       "s3:GetBucketVersioning",
+       "s3:ListObjects"
       ],
       "Resource": "${aws_s3_bucket.bucket.arn}",
       "Effect": "Allow"
