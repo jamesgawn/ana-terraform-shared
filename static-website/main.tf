eb64a1eb64f4c0f12abd5044e0752af659e20214
akyan <james@gawn.uk>
2018-10-20 21:52:31 +0100

    Added configuration for cloudwatch to host S3 content using custom certificate.

--- static-website/main.tf
+++ static-website/main.tf
@@ -6,6 +6,15 @@ variable "cert-domain" {
   type = "string"
 }
 
+variable "site-domains" {
+  type = "list"
+}
+
+variable "root" {
+  type = "string"
+  description = "The default file to return when accessing the root of the domain."
+}
+
 // The AWS Cert Manager for globally managed domain names
 data "aws_acm_certificate" "cert" {
   provider = "aws.us-east-1"
@@ -16,4 +25,89 @@ data "aws_acm_certificate" "cert" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.site-name}"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Identity for ${var.site-name}"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.bucket.arn}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "access_policy" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  policy = "${data.aws_iam_policy_document.s3_policy.json}"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.bucket.bucket_domain_name}"
+    origin_id   = "${var.site-name}"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "${var.root}"
+
+
+  aliases = "${var.site-domains}"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.site-name}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress = true
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  tags {
+    Environment = "production"
+  }
+
+  restrictions {
+    "geo_restriction" {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
+  }
 }
