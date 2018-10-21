abc1643d89af479a87c06a96964fb02f3cf27a79
akyan <james@gawn.uk>
2018-10-21 12:12:08 +0100

    Split out cloudwatch/S3 distribution from static website with CI/CD.

--- static-website/main.tf
+++ static-website/main.tf
@@ -19,99 +19,13 @@ variable "github-repo" {
   type = "string"
 }
 
-// The AWS Cert Manager for globally managed domain names
-data "aws_acm_certificate" "cert" {
-  provider = "aws.us-east-1"
+module "cd" {
+  source = "../cloudfront-distribution-via-s3"
 
-  domain   = "${var.cert-domain}"
-  statuses = ["ISSUED"]
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.site-name}"
-}
-
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "Identity for ${var.site-name}"
-}
-
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.bucket.arn}"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "access_policy" {
-  bucket = "${aws_s3_bucket.bucket.id}"
-  policy = "${data.aws_iam_policy_document.s3_policy.json}"
-}
-
-resource "aws_cloudfront_distribution" "distribution" {
-  origin {
-    domain_name = "${aws_s3_bucket.bucket.bucket_domain_name}"
-    origin_id   = "${var.site-name}"
-
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "${var.root}"
-
-
-  aliases = "${var.site-domains}"
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${var.site-name}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    compress = true
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    "geo_restriction" {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = "${data.aws_acm_certificate.cert.arn}"
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1.1_2016"
-  }
+  site-name = "${var.site-name}"
+  cert-domain = "${var.cert-domain}"
+  site-domains = "${var.site-domains}"
+  root = "${var.root}"
 }
 
 resource "aws_iam_role" "codebuild_assume_role" {
@@ -151,7 +65,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
        "s3:DeleteObject",
        "s3:DeleteObjectVersion"
       ],
-      "Resource": ["${aws_s3_bucket.bucket.arn}","${aws_s3_bucket.bucket.arn}/*"],
+      "Resource": ["${module.cd.s3_bucket_arn}","${module.cd.s3_bucket_arn}/*"],
       "Effect": "Allow"
     },
     {
@@ -212,9 +126,9 @@ resource "aws_codebuild_webhook" "build_webhook" {
 }
 
 output "domain_name" {
-  value = "${aws_cloudfront_distribution.distribution.domain_name}"
+  value = "${module.cd.domain_name}"
 }
 
 output "hosted_zone_id" {
-  value = "${aws_cloudfront_distribution.distribution.hosted_zone_id}"
+  value = "${module.cd.hosted_zone_id}"
 }
